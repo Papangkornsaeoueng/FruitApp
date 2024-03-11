7755662c9dd85c0c116c918ceda65a1d5db3a821
Papangkornsaeoueng <papangkornsaeoueng2@gmail.com>
2024-03-12 06:29:55 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/welcome_059/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.welcome_059
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/FruitDataModel.dart
@@ -0,0 +1,5 @@
+//652021059 ปภังกร แซ่อึ่ง
+class FruitDataModel {
+  final String name, ImageUrl, desc;
+  FruitDataModel(this.name, this.ImageUrl, this.desc);
+}
